c5ab8f51b27795ca0edf3283b12a71e3200e649c
Elijah Cobb <elijahjcobb@gmail.com>
2023-12-03 10:32:09 -0800

    add icons

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 import { ImNpm } from "react-icons/im";
+import Image from 'next/image';
+import icon from "./public/icon.svg";
 
 
 const config: DocsThemeConfig = {
-  logo: <span>type-guarder</span>,
+  logo: <div style={{
+    display: 'flex',
+    alignItems: 'center',
+    gap: '8px',
+  }}>
+    <Image src={icon} alt="icon" width={32} />
+    <span>type-guarder</span>
+  </div>,
   chat: {
     link: 'https://www.npmjs.com/package/type-guarder',
     icon: <ImNpm size={24} color="#CC3534" />
@@ -16,6 +25,17 @@ const config: DocsThemeConfig = {
   footer: {
     text: 'type-guarder',
   },
+  head: (
+    <>
+      <title>type-guarder</title>
+      <link href="https://type-guarder.elijahcobb.app/icon.svg" rel="icon" type="image/svg+xml" />
+      <meta property="og:title" content="type-guarder" />
+      <meta property="description" content="A simple, powerful and flexible runtime type checker for TypeScript." />
+      <meta property="og:description" content="A simple, powerful and flexible runtime type checker for TypeScript." />
+      <meta property="og:url" content="type-guarder.elijahcobb.app" />
+    </>
+
+  )
 }
 
 export default config
